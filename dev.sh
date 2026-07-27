22702cded9658438f0666bbab3aaa8fcc4104ec2
wangchuanhui <372020909@qq.com>
2026-07-27 14:35:17 +0800

    dev

--- dev.sh
+++ dev.sh
@@ -1,2 +1,5 @@
 rm ../../hass-config/debug/www/community/lovelace-wallpanel/wallpanel.js.gz
+rm ../../hass-config/test_313/www/community/lovelace-wallpanel/wallpanel.js.gz
 cp wallpanel-src.js ../../hass-config/debug/www/community/lovelace-wallpanel/wallpanel.js
+cp wallpanel-src.js ../../hass-config/test_313/www/community/lovelace-wallpanel/wallpanel.js
+npm run build
